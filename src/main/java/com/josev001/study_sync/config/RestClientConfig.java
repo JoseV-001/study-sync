241ff99a4cf5af4cf26b737b4f9af5050d8885c1
José victor <Leaphaz@gmail.com>
2026-08-12 23:44:22 -0300

    feat: configure Clockify RestClient

--- src/main/java/com/josev001/study_sync/config/RestClientConfig.java
+++ src/main/java/com/josev001/study_sync/config/RestClientConfig.java
@@ -1,5 +1,19 @@
 package com.josev001.study_sync.config;
 
 // Classe de configuração usada para expor os objetos de apoio ao restante da aplicação.
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+
+@Configuration // -> Indica que vai definir beans no contexto da aplicação
 public class RestClientConfig {
+
+    @Bean
+    public RestClient restClient(ClockifyProperties properties) {
+        return RestClient.builder()
+                .baseUrl(properties.getBaseUrl())
+                .build();
+    }
+
 }
