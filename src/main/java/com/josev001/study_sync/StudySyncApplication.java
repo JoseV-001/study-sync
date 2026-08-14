6cc307fd32b0dbe79ce8d8834cb7abf5e7aa3cd6
José victor <Leaphaz@gmail.com>
2026-08-13 21:33:48 -0300

    feat: add Clockify user integration

--- src/main/java/com/josev001/study_sync/StudySyncApplication.java
+++ src/main/java/com/josev001/study_sync/StudySyncApplication.java
@@ -1,8 +1,11 @@
 package com.josev001.study_sync;
 
+import com.josev001.study_sync.client.ClockifyClient;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
 
 // Classe principal da aplicação Spring Boot que inicializa o contexto da aplicação.
 @ConfigurationPropertiesScan
@@ -11,6 +14,13 @@ public class StudySyncApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudySyncApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner testClockify(ClockifyClient clockifyClient) {
+		return args -> {
+			System.out.println("Usuário do Clockify: " + clockifyClient.getUser());
+		};
 	}
 
 }
--- src/main/java/com/josev001/study_sync/client/ClockifyClient.java
+++ src/main/java/com/josev001/study_sync/client/ClockifyClient.java
@@ -1,14 +1,28 @@
 package com.josev001.study_sync.client;
 
 import com.josev001.study_sync.config.ClockifyProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
 
 // Cliente responsável por centralizar a integração com o Clockify.
+
+@Component// -> Faz o spring gerenciar o ClockifyClient
 public class ClockifyClient {
 
+    private final RestClient restClient;
     private final ClockifyProperties properties;
 
-    public ClockifyClient(ClockifyProperties properties) {
+    public ClockifyClient(RestClient restClient, ClockifyProperties properties) {
+        this.restClient = restClient;
         this.properties = properties;
+    }
+
+    public String getUser() {
+        return restClient.get()
+                .uri("/user")
+                .header("X-Api-Key", properties.getApiKey())
+                .retrieve()
+                .body(String.class);
     }
 
 }
