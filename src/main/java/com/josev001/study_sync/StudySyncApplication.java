72ce9e26c814879d0054f33988d80fb352ab0487
José victor <Leaphaz@gmail.com>
2026-08-18 19:53:40 -0300

    feat: calculate current week study time

--- src/main/java/com/josev001/study_sync/StudySyncApplication.java
+++ src/main/java/com/josev001/study_sync/StudySyncApplication.java
@@ -1,6 +1,6 @@
 package com.josev001.study_sync;
 
-import com.josev001.study_sync.dto.TimeEntryDto;
+
 import com.josev001.study_sync.service.ClockifyService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Bean;
 
-import java.util.List;
+import java.time.Duration;
 
 // Classe principal da aplicação Spring Boot que inicializa o contexto da aplicação.
 @ConfigurationPropertiesScan
@@ -20,20 +20,12 @@ public class StudySyncApplication {
     }
 
     @Bean
-    CommandLineRunner testClockify(ClockifyService clockifyService) {
+    CommandLineRunner testWeeklyTotal(ClockifyService clockifyService) {
         return args -> {
-            List<TimeEntryDto> entries = clockifyService.getTimeEntries();
+            Duration total = clockifyService.getTotalStudyTime();
 
-            System.out.println("Quantidade de registros: " + entries.size());
-
-            entries.stream()
-                    .limit(5)
-                    .forEach(entry ->
-                            System.out.println(
-                                    entry.description() + " - " +
-                                            entry.timeInterval().duration()
-                            )
-                    );
+            System.out.println("Total da semana: " + total);
         };
     }
+
 }
--- src/main/java/com/josev001/study_sync/service/ClockifyService.java
+++ src/main/java/com/josev001/study_sync/service/ClockifyService.java
@@ -3,8 +3,13 @@ package com.josev001.study_sync.service;
 import com.josev001.study_sync.client.ClockifyClient;
 import com.josev001.study_sync.dto.TimeEntryDto;
 import org.springframework.stereotype.Service;
-
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.Duration;
 import java.util.List;
+import java.time.DayOfWeek;
+import java.time.temporal.TemporalAdjusters;
 
 @Service
 public class ClockifyService {
@@ -19,4 +24,45 @@ public class ClockifyService {
         return clockifyClient.getTimeEntries();
     }
 
+    // Soma a duração dos registros finalizados da semana atual.
+    public Duration getTotalStudyTime() {
+
+        LocalDate startOfWeek = getStartOfCurrentWeek();
+        LocalDate endOfWeek = getEndOfCurrentWeek();
+
+        return clockifyClient.getTimeEntries()
+                .stream()
+                .filter(entry -> entry.timeInterval().duration() != null)
+                .filter(entry -> {
+                    LocalDate entryDate = getEntryDate(entry);
+
+                    return !entryDate.isBefore(startOfWeek)//A data do registro não pode ser anterior à segunda-feira da semana atual
+                            && !entryDate.isAfter(endOfWeek);// a data não pode ser depois do domingo
+                    //segunda <= data <= domingo
+                })
+                .map(entry -> Duration.parse(entry.timeInterval().duration()))
+                .reduce(Duration.ZERO, Duration::plus);
+    }
+
+    // Converte a data/hora UTC recebida do Clockify para uma data no fuso de São Paulo.
+    private LocalDate getEntryDate(TimeEntryDto entry) {
+
+        Instant start = Instant.parse(entry.timeInterval().start());
+
+        return start
+                .atZone(ZoneId.of("America/Sao_Paulo"))
+                .toLocalDate();
+    }
+
+    // Retorna a segunda-feira da semana atual.
+    private LocalDate getStartOfCurrentWeek() {
+        return LocalDate.now(ZoneId.of("America/Sao_Paulo"))
+                .with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY));
+    }
+
+    // Retorna o domingo da semana atual.
+    private LocalDate getEndOfCurrentWeek() {
+        return LocalDate.now(ZoneId.of("America/Sao_Paulo"))
+                .with(TemporalAdjusters.nextOrSame(DayOfWeek.SUNDAY));
+    }
 }
